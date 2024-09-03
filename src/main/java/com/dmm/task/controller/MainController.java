166a23150df11e63be57217282cf171ddcdea712
NaeSakai <170297507+mamedaihu@users.noreply.github.com>
2024-09-03 23:56:28 +0900

    9/3コミット

--- src/main/java/com/dmm/task/controller/MainController.java
+++ src/main/java/com/dmm/task/controller/MainController.java
@@ -10,6 +10,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +29,7 @@ public class MainController {
 	
 	//カレンダー表示
 	@GetMapping("/main")
-	public String main() {
+	public String main(Model model, @AuthenticationPrincipal AccountUserDetails user) {
 		//	週と日を格納する二次元配列を用意する
 		List<List<LocalDate>> month = new ArrayList<>();
 		
@@ -35,7 +37,7 @@ public class MainController {
 		List<LocalDate> week = new ArrayList<>();
 		
 		//日にちを格納する変数を用意する
-		LocalDate day;
+		LocalDate day, start, end;
 		
 		//その月の1日を取得する
 		day = LocalDate.now(); //現在日時を取得
@@ -44,6 +46,7 @@ public class MainController {
 		//前月分のLocalDateを求める
 		DayOfWeek w = day.getDayOfWeek(); //当該日の曜日を取得
 		day = day.minusDays(w.getValue()); //1日からマイナス
+		start = day;
 		
 		//1週目（1日ずつ増やして 週のリストに格納していく）
 		for(int i = 1; i <= 7; i++) {
@@ -74,6 +77,32 @@ public class MainController {
 			day = day.plusDays(1);
 		}
 		
+		end = day;
+		
+		//日付とタスクを紐づけるコレクション
+		MultiValueMap<LocalDate, Tasks> tasks = new LinkedMultiValueMap<LocalDate, Tasks>();
+		
+		List<Tasks> list;
+		
+		//管理者だったら
+		if(user.getUsername().equals("admin")) {
+			list = repo.findAllByDateBetween(start.atTime(0, 0), end.atTime(0, 0));
+		} else { //ユーザーだったら
+			list = repo.findByDateBetween(start.atTime(0, 0), end.atTime(0, 0), user.getName());
+		}
+		
+		// 取得したタスクをコレクションに追加
+		for (Tasks task : list) {
+			tasks.add(task.getDate().toLocalDate(), task);
+		}
+		
+		//カレンダーのデータをHTMLに連携
+		model.addAttribute("matrix", month);
+		
+		//コレクションのデータをHTMLに連携
+		model.addAttribute("tasks", tasks);
+		
+		//HTML表示
 		return "main";
 	}
 	
@@ -96,5 +125,8 @@ public class MainController {
 		
 		return "redirect:/main";
 	}
+	
+	
+	
 
 }
--- src/main/java/com/dmm/task/repository/TasksRepository.java
+++ src/main/java/com/dmm/task/repository/TasksRepository.java
@@ -1,11 +1,22 @@
 package com.dmm.task.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dmm.task.enttity.Tasks;
 
 public interface TasksRepository extends JpaRepository<Tasks, Integer>, JpaSpecificationExecutor<Tasks>{
 	
-
+	//ユーザー用
+	@Query("select a from Tasks a where a.date between :from and :to and name = :name")
+	List<Tasks> findByDateBetween(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to, @Param("name") String name);
+	
+	//管理者用
+	@Query("select a from Tasks a where a.date between :from and :to")
+	List<Tasks> findAllByDateBetween(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to);
 }
